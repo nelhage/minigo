be1cac422d20289b6a1917b9c2368b81fdf74759
Nelson Elhage <nelhage@nelhage.com>
2016-03-20 16:44:36 -0700

    Better floodfill tests

--- game/board_test.go
+++ game/board_test.go
@@ -1,6 +1,49 @@
 package game
 
-import "testing"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+
+	"nelhage.com/minigo/bit"
+)
+
+var fixtureRE = regexp.MustCompile(`\A\s*((?:\d+\s*(?:[OX+*]\s*)+\n)+)`)
+
+func board(g *Game, in string) *boardState {
+	white := bit.NewVector(g.size * g.size)
+	black := bit.NewVector(g.size * g.size)
+
+	m := fixtureRE.FindStringSubmatch(in)
+	if m == nil {
+		panic(fmt.Sprintf("bad fixture:\n%s", in))
+	}
+	lines := strings.Split(strings.TrimRight(m[1], "\n"), "\n")
+	if len(lines) != g.size {
+		panic(fmt.Sprintf("bad fixture (%d rows):\n%#v", len(lines), lines))
+	}
+	for i, l := range lines {
+		bits := strings.Split(l, " ")
+		if len(bits) != g.size+1 {
+			panic(fmt.Sprintf("bad fixture:\n%s", in))
+		}
+		for j, c := range bits[1:] {
+			switch c {
+			case "*", "+":
+			case "O":
+				white.Set(g.size*i + j)
+			case "X":
+				black.Set(g.size*i + j)
+			}
+		}
+	}
+	return &boardState{
+		g:     g,
+		white: white,
+		black: black,
+	}
+}
 
 func TestBasic(t *testing.T) {
 	g := New(9)
@@ -48,95 +91,92 @@ func TestBasic(t *testing.T) {
 }
 
 func TestFloodFill(t *testing.T) {
-	g := New(9)
-	root := g.board.white.Copy().Set(4*9 + 4)
-	flood := g.board.floodFill(root, g.board.white)
-	if !flood.Equal(g.z.Copy().Not()) {
-		b := &boardState{
-			black: flood,
-			white: g.board.white,
-			g:     g,
+	cases := []struct{ in, out string }{
+		{`
+0 + + + + + + + + +
+1 + + + + + + + + +
+2 + + * + + + * + +
+3 + + + + + + + + +
+4 + + + + O + + + +
+5 + + + + + + + + +
+6 + + * + + + * + +
+7 + + + + + + + + +
+8 + + + + + + + + +
+  0 1 2 3 4 5 6 7 8
+`, `
+0 O O O O O O O O O
+1 O O O O O O O O O
+2 O O O O O O O O O
+3 O O O O O O O O O
+4 O O O O O O O O O
+5 O O O O O O O O O
+6 O O O O O O O O O
+7 O O O O O O O O O
+8 O O O O O O O O O
+  0 1 2 3 4 5 6 7 8
+`,
+		},
+		{`
+0 + + + + + + + + +
+1 + + + + + + + + +
+2 + + * X X X * + +
+3 + + X + + + X + +
+4 + + X + O + X + +
+5 + + X + + + X + +
+6 + + * X X X * + +
+7 + + + + + + + + +
+8 + + + + + + + + +
+  0 1 2 3 4 5 6 7 8
+`, `
+0 + + + + + + + + +
+1 + + + + + + + + +
+2 + + * X X X * + +
+3 + + X O O O X + +
+4 + + X O O O X + +
+5 + + X O O O X + +
+6 + + * X X X * + +
+7 + + + + + + + + +
+8 + + + + + + + + +
+  0 1 2 3 4 5 6 7 8
+`,
+		},
+		{`
+0 + + + X O X + + +
+1 + + + + X + + + +
+2 + + * + + + * + +
+3 X + + + + + + + X
+4 O X + + + + + X O
+5 O + + + + + + + X
+6 + + * X + + * + +
+7 + + X + X + + + +
+8 + X + O + X + + +
+  0 1 2 3 4 5 6 7 8
+`, `
+0 + + + X O X + + +
+1 + + + + X + + + +
+2 + + * + + + * + +
+3 X + + + + + + + X
+4 O X + + + + + X O
+5 O + + + + + + + X
+6 + + * X + + * + +
+7 + + X O X + + + +
+8 + X O O O X + + +
+  0 1 2 3 4 5 6 7 8
+`,
+		},
+	}
+	for _, tc := range cases {
+		g := New(9)
+		in := board(g, tc.in)
+		out := board(g, tc.out)
+
+		fill := in.floodFill(in.white, in.black)
+		if !fill.Equal(out.white) {
+			t.Logf("fill=%#v want=%#v", fill, out.white)
+			t.Logf("count(fill)=%d count(want)=%d", fill.Popcount(), out.white.Popcount())
+			t.Errorf("wrong fill in=\n%s\nwant=\n%s\nout=\n%s",
+				in, out,
+				&boardState{white: fill, black: out.black, g: g})
 		}
-		t.Errorf("flood fill did not fill:\n%s", b.String())
-	}
-
-	bounds := g.board.white.Copy()
-	for i := 2; i < 7; i++ {
-		bounds.Set(2*g.size + i)
-		bounds.Set(i*g.size + 2)
-		bounds.Set(6*g.size + i)
-		bounds.Set(i*g.size + 6)
-	}
-
-	flood = g.board.floodFill(root, bounds)
-	b := &boardState{
-		black: flood,
-		white: bounds,
-		g:     g,
-	}
-	for i := 0; i < 2; i++ {
-		for j := 0; j < g.size; j++ {
-			check := []struct{ x, y int }{
-				{i, j},
-				{g.size - i, j},
-				{j, i},
-				{g.size - j, i},
-			}
-			for _, ch := range check {
-				if _, ok := b.at(ch.x, ch.y); ok {
-					t.Fatalf("At(%d,%d):\n%s",
-						ch.x, ch.y, b)
-				}
-			}
-		}
-	}
-
-	/*
-	 0 + + + X O X + + +
-	 1 + + + + X + + + +
-	 2 + + * + + + * + +
-	 3 X + + + + + + + X
-	 4 O X + + + + + X O
-	 5 X + + + + + + + X
-	 6 + + * + + + * + +
-	 7 + + + + X + + + +
-	 8 + + + X O X + + +
-	   0 1 2 3 4 5 6 7 8
-	*/
-	root = g.board.white.Copy()
-	root.Set(4*g.size + 8)
-	root.Set(4 * g.size)
-	root.Set(4)
-	root.Set(g.size*(g.size-1) + 4)
-
-	bounds = g.board.white.Copy()
-	bounds.Set(3)
-	bounds.Set(5)
-	bounds.Set(g.size + 4)
-
-	bounds.Set(3 * g.size)
-	bounds.Set(3*g.size + g.size - 1)
-	bounds.Set(4*g.size + 1)
-	bounds.Set(4*g.size + g.size - 2)
-	bounds.Set(5 * g.size)
-	bounds.Set(5*g.size + g.size - 1)
-
-	bounds.Set(7*g.size + 4)
-	bounds.Set((g.size-1)*g.size + 3)
-	bounds.Set((g.size-1)*g.size + 5)
-
-	t.Logf("test fill:\n%s", &boardState{
-		white: root,
-		black: bounds,
-		g:     g})
-
-	flood = g.board.floodFill(root, bounds)
-	b = &boardState{
-		white: flood,
-		black: bounds,
-		g:     g,
-	}
-	if flood.Popcount() != 4 {
-		t.Errorf("popcount!=4\n%s", b)
 	}
 }
